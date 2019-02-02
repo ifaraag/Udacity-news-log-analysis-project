902c67b4cefbf41162db30de707a247937a8f0cb
Ibrahim Emad Farrag <ibrahim.emad.farrag@gmail.com>
2019-02-02 08:30:37 +0200

    error fixs

--- news_logs_analysis.py
+++ news_logs_analysis.py
@@ -4,6 +4,7 @@
 # improt database PostgreSQl lib
 import psycopg2
 
+
 # Database queries: SQL Queries to get or extract the required information from the newsdata.sql database 
 #-------------------------------------------------------------------------------------------------------
 
@@ -22,7 +23,7 @@ query_authors_popularity = """select authors.name,count (*) as count
 			order by count desc;"""
 # Query 3: On which day did more than 1% of requests lead to errors?
 query_bad_requests = """select *
-			from ( select total_request.day, total_req, num_failed_req, (num_failed_req::float/total_req::float) * 100 as precent 
+			from ( select total_request.day, (num_failed_req::float/total_req::float) * 100 as precent 
 			from failed_request, total_request 
 			where failed_request.day=total_request.day ) as error 
 			where  precent  > 1;"""
@@ -36,12 +37,12 @@ def query_database(sql_query_request):
 		cursor = connection.cursor()
 	except:
 		print("Failed to connect to the PostgreSQL database.")
-        return None
-    else:
-    	cursor.execute(sql_query_request)
-    	results = cursor.fetchall()
-    	connection.close()
-    return results
+		return None
+	else:
+		cursor.execute(sql_query_request)
+		results = cursor.fetchall()
+		connection.close()
+		return results
 #-------------------------------------------------------------------------------------------------------
 
 # Report Output
@@ -50,33 +51,33 @@ def query_database(sql_query_request):
 # ------------------------------------------------------------------------------------------------------
 def popular_articles():
 	popular_3_articles = query_database(query_popular_articles)
-    print("\n\t\t The Three Most Popular Articles \n")
-    for title, count in popular_3_articles:
-        print(" \"{}\" -- {} views".format(title, count))
+	print("\n\t\t The Three Most Popular Articles \n")
+	for title, count in popular_3_articles:
+		print(" \"{}\" -- {} views".format(title, count))
 # ------------------------------------------------------------------------------------------------------
        
 
 # Print the number of viwes per author 
 # ------------------------------------------------------------------------------------------------------
 def authors_popularity():
-    authors_popularity = query_database(query_authors_popularity)
-    print("\n\t\t Number of Viwes Per Author \n")
-    for name, count in authors_popularity:
-        print(" {} -- {} views".format(name, count))
+	authors_popularity = query_database(query_authors_popularity)
+	print("\n\t\t Number of Viwes Per Author \n")
+	for name, count in authors_popularity:
+		print(" {} -- {} views".format(name, count))
 # ------------------------------------------------------------------------------------------------------
 
 
 # Print days with more than 1% bad requests
 # ------------------------------------------------------------------------------------------------------
 def bad_requests():
-    bad_requests = query_database(query_bad_requests)
-    print("\n\t\t Days with more than one percentage of bad requests \n")
-    for day, precent in bad_requests:
-        print("""{0:%B %d, %Y} -- {1:.2f} % errors""".format(day, precent))
+	bad_requests = query_database(query_bad_requests)
+	print("\n\t\t Days with more than one percentage of bad requests \n")
+	for day, precent in bad_requests:
+		print("""{0:%B %d, %Y} -- {1:.2f} % errors""".format(day, precent))
 # ------------------------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
-    popular_articles()
-    authors_popularity()
-    bad_requests()
+	popular_articles()
+	authors_popularity()
+	bad_requests()
