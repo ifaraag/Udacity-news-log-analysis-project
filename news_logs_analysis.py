0205d669576745916f556b970f4f78a62d3eaa82
Ibrahim Emad Farrag <ibrahim.emad.farrag@gmail.com>
2019-02-02 04:29:20 +0200

    Initial Commit

---
+++ news_logs_analysis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# improt database PostgreSQl lib
+import psycopg2
+
+# Database queries: SQL Queries to get or extract the required information from the newsdata.sql database 
+#-------------------------------------------------------------------------------------------------------
+# Query 1:What are the three most popular articles of all time? 
+
+
+# Database query 2: Who are the most popular article authors of all time?
+
+# Database query 3: On which day did more than 1% of requests lead to errors?
+
+#-------------------------------------------------------------------------------------------------------
