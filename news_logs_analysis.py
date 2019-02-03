34d138a9d7770d8bc6d24500d6a6041273e57ddb
Ibrahim Emad Farrag <ibrahim.emad.farrag@gmail.com>
2019-02-04 00:18:06 +0200

    remove wildcards

--- news_logs_analysis.py
+++ news_logs_analysis.py
@@ -10,15 +10,18 @@ import psycopg2
 
 # Query 1:What are the three most popular articles of all time?
 query_popular_articles = """select articles.title,count (*) as count
-            from articles join log on '/article/' || articles.slug like log.path
-            where status like '%200%'
+            from articles , log
+            where'/article/' || articles.slug = log.path
+            and status = '200 OK'
             group by articles.title
             order by count desc
             limit 3;"""
 # Query 2: Which authors get the most page views? Each author Read counts ?
 query_authors_popularity = """select authors.name,count (*) as count
-            from articles, log ,authors where '/article/' || articles.slug like log.path
-            and log.status like '%200%' and articles.author=authors.id
+            from articles, log ,authors
+            where '/article/' || articles.slug = log.path
+            and log.status = '200 OK'
+            and articles.author=authors.id
             group by authors.name
             order by count desc;"""
 # Query 3: On which day did more than 1% of requests lead to errors?
@@ -29,8 +32,11 @@ query_bad_requests = """select *
             where  precent  > 1;"""
 # -------------------------------------------------------------------------------------------------------
 
-# Open connection to database, Query data from the database, Return results and Close the connection
+# Open connection to database, Query data from the database, Return results
+# and Close the connection
 # -------------------------------------------------------------------------------------------------------
+
+
 def query_database(sql_query_request):
     try:
         connection = psycopg2.connect(database="news")
